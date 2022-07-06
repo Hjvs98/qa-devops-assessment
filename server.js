cc535690b903deecfe04637c381196b6d3e77551
Hunter Saunders <hjvs98@gmail.com>
2022-07-06 07:49:32 -0600

    ready for deployment

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const { bots, playerRecord } = require("./data");
+const { shuffleArray } = require("./utils");
+
+app.use(express.json());
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: "1ee4401a29454949b89edc5ca7fbae39",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/api/robots", (req, res) => {
+  try {
+    res.status(200).send(botsArr); //BUG ON THIS LINE (botsArr not declared nor initialized)
+  } catch (error) {
+    rollbar.error("Error getting bots from GET /api/robots endpoint");
+    console.log("ERROR GETTING BOTS", error);
+    res.sendStatus(400);
+  }
+});
+
+app.get("/api/robots/five", (req, res) => {
+  try {
+    let shuffled = shuffleArray(bots);
+    let choices = shuffled.slice(0, 5);
+    let compDuo = shuffled.slice(6, 8);
+    res.status(200).send({ choices, compDuo });
+    rollbar.info("Successfully drew five cards for user");
+  } catch (error) {
+    rollbar.error("Error drawing five bots from GET /api/robots/five endpoint");
+    console.log("ERROR GETTING FIVE BOTS", error);
+    res.sendStatus(400);
+  }
+});
+
+app.post("/api/duel", (req, res) => {
+  try {
+    // getting the duos from the front end
+    let { compDuo, playerDuo } = req.body;
+
+    // adding up the computer player's total health and attack damage
+    let compHealth = compDuo[0].health + compDuo[1].health;
+    let compAttack =
+      compDuo[0].attacks[0].damage +
+      compDuo[0].attacks[1].damage +
+      compDuo[1].attacks[0].damage +
+      compDuo[1].attacks[1].damage;
+
+    // adding up the player's total health and attack damage
+    let playerHealth = playerDuo[0].health + playerDuo[1].health;
+    let playerAttack =
+      playerDuo[0].attacks[0].damage +
+      playerDuo[0].attacks[1].damage +
+      playerDuo[1].attacks[0].damage +
+      playerDuo[1].attacks[1].damage;
+
+    // calculating how much health is left after the attacks on each other
+    let compHealthAfterAttack = compHealth - playerAttack;
+    let playerHealthAfterAttack = playerHealth - compAttack;
+
+    // comparing the total health to determine a winner
+    if (compHealthAfterAttack > playerHealthAfterAttack) {
+      playerRecord.losses++;
+      res.status(200).send("You lost!");
+    } else {
+      playerRecord.losses++; //BUG (TYPO) ON THIS LINE!
+      rollbar.warn("Losses incremented even though user won!");
+      res.status(200).send("You won!");
+    }
+  } catch (error) {
+    rollbar.error("Error dueling at POST /api/duel endpoint");
+    console.log("ERROR DUELING", error);
+    res.sendStatus(400);
+  }
+});
+
+app.get("/api/player", (req, res) => {
+  try {
+    res.status(200).send(playerRecord);
+    rollbar.info("User record may be incorrect. Successfully sent record.");
+  } catch (error) {
+    console.log("ERROR GETTING PLAYER STATS", error);
+    res.sendStatus(400);
+  }
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
