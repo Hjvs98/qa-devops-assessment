28d3b44c7ab2120307b2a1a571b4fe9fee02e813
Hunter Saunders <hjvs98@gmail.com>
2022-07-06 08:04:22 -0600

    tests

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -6,7 +6,7 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
+    await driver.get('http://localhost:3000/')
 })
 
 afterAll(async () => {
@@ -17,4 +17,17 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+})
+
+test('Choices div is not displayed before clicking "Draw"', async () => {
+    const div = await driver.findElement(By.id('choices'));
+    const displayed = await div.isDisplayed();
+    expect(displayed).toBe(false);
+})
+
+test('Draw button displays choices div', async () => {
+    await driver.findElement(By.id('draw')).click();
+    const div = await driver.findElement(By.id('choices'));
+    const displayed = await div.isDisplayed();
+    expect(displayed).toBe(true);
 })
